b570071802c42d5ad011e78896b6794186ce7b95
vzago <vitorzago94@gmail.com>
2025-01-19 15:05:25 -0300

    Field Validators, overriding save method from Model and resizing images

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validators import validate_png
+from utils.images import resize_image
 
 class MenuLink(models.Model):
     class Meta:
@@ -37,7 +39,19 @@ class SiteSetup(models.Model):
     favicon = models.ImageField(
         upload_to='assets/favicon/%Y/%m/',
         blank=True, default='', null=True,
+        validators=[validate_png],
     )
+    
+    def save(self, *args, **kwargs):
+        current_favicon_name = str(self.favicon.name)
+        super().save(*args, **kwargs)
+        favicon_changed = False
+        
+        if self.favicon:
+            favicon_changed =  current_favicon_name != self.favicon.name
+        
+        if favicon_changed:
+            resize_image(self.favicon, 32)
     
     def __str__(self):
         return self.title
