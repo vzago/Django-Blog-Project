d1da262f6cf9018b350b4fc249064bd31282363b
vzago <vitorzago94@gmail.com>
2025-01-23 21:53:32 -0300

    Migrating index view to django.views.generic.list.ListView Class Based View

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from blog.views import index, post, page, created_by, category, tag, search
+from blog.views import post, page, created_by, category, tag, search, PostListView  
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
