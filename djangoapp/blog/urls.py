ef01930e290698f2f12e47ccd0fe4b53602a3335
vzago <vitorzago94@gmail.com>
2025-01-23 22:37:58 -0300

    Using self.kwargs for URL arguments and method calling order in cbv

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import post, page, created_by, category, tag, search, PostListView  
+from blog.views import post, page, CreatedByListView, category, tag, search, PostListView  
 
 app_name = 'blog'
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,18 +1,18 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from blog.models import Post, Page
-from django.db.models import Q
-from django.contrib.auth.models import User
-from django.http import Http404
-from django.views.generic import ListView
+from typing import Any
 
+from blog.models import Page, Post
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.views.generic import ListView
 PER_PAGE = 9
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()
     
@@ -29,24 +29,7 @@ class PostListView(ListView):
         })
 
         return context
-
-
-# def index(request):
-#     posts = Post.objects.get_published()
     
-#     paginator = Paginator(posts, PER_PAGE)    
-#     page_number = request.GET.get("page")
-#     page_obj = paginator.get_page(page_number)
-
-#     return render(
-#         request,
-#         'blog/pages/index.html',
-#         {
-#             'page_obj': page_obj,
-#             'page_title': 'Home - ',
-#         }
-#     )
-
 def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first()
     
@@ -75,6 +58,46 @@ def created_by(request, author_pk):
             'page_title': page_title,
         }
     )
+    
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._temp_context: dict[str, Any] = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = 'Posts de ' + user_full_name + ' - '
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
+
     
 def category(request, slug):
     posts = Post.objects.get_published()\
