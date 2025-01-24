9a7b38ee6fc2a8f065373cb42d5f2ed7de0ac711
vzago <vitorzago94@gmail.com>
2025-01-24 10:53:09 -0300

    Migrating Page view to DetailView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import post, page, CreatedByListView, CategoryListView, TagListView, SearchListView, PostListView  
+from blog.views import post, PageDetailView, CreatedByListView, CategoryListView, TagListView, SearchListView, PostListView  
 
 app_name = 'blog'
 
@@ -7,7 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.http import Http404
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 PER_PAGE = 9
 
 class PostListView(ListView):
@@ -136,55 +136,26 @@ class SearchListView(PostListView):
             return redirect('blog:index')
         return super().get(request, *args, **kwargs)    
     
+class PageDetailView(DetailView):
+    model = Page
+    template_name = 'blog/pages/page.html'
+    slug_field = 'slug'
+    context_object_name = 'page'
     
-def search(request):
-    search_value = request.GET.get('search', '').strip()
-    
-    posts = (
-        Post.objects.get_published()
-        .filter(
-            Q(title__icontains=search_value) |
-            Q(excerpt__icontains=search_value) |
-            Q(content__icontains=search_value)
-        )[:PER_PAGE]
-    )
-    
-    page_title =  f'{search_value[:30]} - Search -'
-    
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'search_value': search_value,
-            'page_title': page_title,
-        }
-    )
-
-
-
-def page(request, slug):
-    page_object = (
-    Page.objects
-    .filter(is_published=True)
-    .filter(slug=slug)
-    .first()
-)
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        page = self.get_object()
+        page_title = f'{page.title} - Página -'
         
-    if page_object is None :
-        raise Http404()
-
-    page_title =  f'{page_object.title} - Página -'
-
-    return render(
-        request,
-        'blog/pages/page.html',
-        {
-            'page': page_object,
+        ctx.update({
             'page_title': page_title,
-        }
-    )
-
+        })
+        
+        return ctx
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=True)
+        
 
 def post(request,slug):
     
