7ea6ea097b63800c2fa59d25e1a07ef698781930
vzago <vitorzago94@gmail.com>
2025-01-24 10:15:19 -0300

    Migrating category view to ListView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import post, page, CreatedByListView, category, tag, search, PostListView  
+from blog.views import post, page, CreatedByListView, CategoryListView, tag, search, PostListView  
 
 app_name = 'blog'
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
 ]
